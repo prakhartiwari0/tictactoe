0adafae343d6918e81b4f8cc06d96777767b4432
Prakhar Tiwari <indianstudentexplolearner@gmail.com>
2023-09-23 09:40:24 +0530

    Basic game logic built purely by me

--- main.js
+++ main.js
@@ -1,42 +1,103 @@
+let playerXmoves = []
+let playerOmoves = []
 
-
-
-
+let currentPlayer;
 
 const gameInputDivs = document.querySelectorAll('.gameInputDiv')
 
 
-const gameBoard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+const currentPlayerSpan = document.querySelector('.currentPlayerSpan')
+const resultSpan = document.querySelector('.resultSpan')
+
+let gameObject = {
+    currentPlayer:'X',
+    playerXmoves:[],
+    playerOmoves:[],
+
+    markedCellsCount: playerOmoves.length+playerXmoves.length,
 
 
 
-// function createCrossCircle
-
-let game = function(){console.log("bruh");}()
+}
 
 
+function changeCurrentPlayer(){
+    if (currentPlayer == undefined) {
+        currentPlayer = 'X'
+    }
+    else if (currentPlayer == 'X') {
+        currentPlayer = 'O'
+    }
+    else{
+        currentPlayer = 'X'
+    }
+
+    currentPlayerSpan.textContent = currentPlayer
+    
+}
+changeCurrentPlayer()
+
+
+
+function resultCalculator(playerMovesArray, name) {
+
+    let winnerArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+    let winner = 'none';
+    winnerArrays.forEach((value, index)=>{
+        let matches = 0
+        value.forEach((num)=>{
+            playerMovesArray.forEach((movePosition)=>{
+                if (num==movePosition) {
+                    matches++
+                }
+            })
+        })
+        if (matches==3) {
+            winner = name
+        }
+        else{
+            matches = 0;
+        }
+    })
+
+    resultSpan.textContent = winner
+    
+}
 
 gameInputDivs.forEach((element, key) => {
-    console.log(element);
-    console.log(key);
+    element.addEventListener('click', ()=>{
 
-    const cross = document.createElement('span')
-    cross.textContent = 'X'
-    cross.classList.add('cross')
+        if (!element.textContent) {
+            element.textContent = currentPlayer
+            let moveLocation = element.getAttribute('data-position')
+            // console.log(moveLocation);
 
-    const circle = document.createElement('span')
-    circle.textContent = 'O'
-    circle.classList.add('circle')
+            if (currentPlayer=='X') {
+                playerXmoves.push(moveLocation)
+                // console.log('X:', playerXmoves);
 
-    if (gameBoard[key] == 'X') {
-        element.appendChild(cross)
-    }
-    else {
-        element.appendChild(circle)
-    }
-    // element.textContent = gameBoard[key]
+                if (playerXmoves.length>=3) {
+                    resultCalculator(playerXmoves, 'X')
+                }
+            }
+            else{
+                playerOmoves.push(moveLocation)
+                // console.log('O:', playerOmoves);
 
+                if (playerOmoves.length>=3) {
+                    resultCalculator(playerOmoves, 'O')
+                }
+            }
 
+            changeCurrentPlayer()
+        }
+
+        console.log(playerOmoves.length + playerXmoves.length);
+        if (playerOmoves.length + playerXmoves.length == 9) {
+            resultSpan.textContent = 'Draw'
+        }
+    })
 
 })
 
